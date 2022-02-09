df0096371e0013b2fc8597b2a6d1443060ae06b1
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2022-02-09 17:26:14 -0300

    Testes do repositório

---
+++ __tests__/repositorio/leilao.test.js
@@ -0,0 +1,75 @@
+import { obtemLeilao, obtemLeiloes } from '../../src/repositorio/leilao';
+import apiLeiloes from '../../src/servicos/apiLeiloes';
+
+const mockLeiloes = [
+  {
+    id: 1,
+    nome: 'Leilão',
+    descricao: 'Descrição do leilão',
+  }
+]
+
+const mockRequisicao = (retorno) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno
+      });
+    }, 100);
+  });
+};
+
+const mockRequisicaoErro = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 100);
+  });
+};
+
+jest.mock('../../src/servicos/apiLeiloes');
+
+describe('repositorio/leilao', () => {
+
+  beforeEach(() => {
+    apiLeiloes.get.mockClear();
+  });
+
+  describe('obtemLeiloes', () => {
+    it('deve retornar um array de leilões', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes));
+
+      const leiloes = await obtemLeiloes();
+
+      expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes');
+      expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
+      expect(leiloes).toEqual(mockLeiloes);
+    });
+
+    it('deve retornar um array vazio caso erro na API', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicaoErro());
+
+      const leiloes = await obtemLeiloes();
+
+      expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes');
+      expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
+      expect(leiloes).toEqual([]);
+    });
+  });
+
+  describe('obtemLeilao', () => {
+    it('deve retornar um leilão', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes[0]));
+
+      const leilao = await obtemLeilao(1);
+      expect(leilao).toEqual(mockLeiloes[0]);
+    });
+
+    it('deve retornar um objeto vazio caso erro na API', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicaoErro());
+
+      const leilao = await obtemLeilao(1);
+      expect(leilao).toEqual({});
+    });
+  });
+});
