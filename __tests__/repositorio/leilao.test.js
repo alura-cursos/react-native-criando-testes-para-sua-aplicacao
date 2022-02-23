315e10db67c502e68a107b32cc5c38e4398763d1
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2022-02-23 16:34:02 -0300

    Aula 3.2 - Customizando a Implementação

--- __tests__/repositorio/leilao.test.js
+++ __tests__/repositorio/leilao.test.js
@@ -1,14 +1,52 @@
 import { obtemLeiloes } from '../../src/repositorio/leilao';
+import apiLeiloes from '../../src/servicos/apiLeiloes';
 
 jest.mock('../../src/servicos/apiLeiloes');
+
+const mockLeiloes = [
+  {
+    id: 1,
+    nome: 'Leilão',
+    descricao: 'Descrição do leilão'
+  }
+];
+
+const mockRequisicao = (retorno) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno
+      })
+    }, 200);
+  });
+}
+
+const mockRequisicaoErro = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 200);
+  });
+}
 
 describe('respositorio/leilao', () => {
 
   describe('obtemLeiloes', () => {
 
     it('deve retornar uma lista de leilões', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes))
+      
       const leiloes = await obtemLeiloes();
-      console.log(leiloes);
+
+      expect(leiloes).toEqual(mockLeiloes);
+    });
+
+    it('deve retornar uma lista vazia quando a requisição falhar', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicaoErro());
+      
+      const leiloes = await obtemLeiloes();
+
+      expect(leiloes).toEqual([]);
     });
 
   });
