220c5dc73f72fb7eaf8d9ddc7194bcb56a6c1535
Natalia Kelim Thiel <natalia.kthiel@gmail.com>
2022-03-02 18:08:34 -0300

    Aula 4.4 - Manipulando o Componente

--- __tests__/telas/Leilao/componentes/EnviaLance.test.js
+++ __tests__/telas/Leilao/componentes/EnviaLance.test.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
-import { ENVIADO } from '../../../../src/negocio/constantes/estadosLance';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { ENVIADO, NAO_ENVIADO } from '../../../../src/negocio/constantes/estadosLance';
 
 import EnviaLance from '../../../../src/telas/Leilao/componentes/EnviaLance';
 
-describe('telas/Leilao/componentes/EnviaLance', () => {
-  it('deve enviar o lance quando o botão for pressionado', () => {
-    const enviaLance = jest.fn(() => new Promisse(resolve => resolve(ENVIADO)))
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
-    const { toJSON } = render(
+describe('telas/Leilao/componentes/EnviaLance', () => {
+  it('deve enviar o lance quando o botão for pressionado', async () => {
+    const enviaLance = jest.fn(() => new Promise(resolve => resolve(ENVIADO)))
+
+    const {
+      getByPlaceholderText, 
+      getByA11yHint,
+      getByText
+    } = render(
       <EnviaLance
         enviaLance={enviaLance}
         cor="blue"
       />
     );
 
-    console.log(toJSON());
+    const input = getByPlaceholderText("R$");
+    const botao = getByA11yHint("Enviar lance");
+
+    fireEvent.changeText(input, "10");
+    fireEvent.press(botao);
+
+    expect(enviaLance).toHaveBeenCalledWith("10");
+    await waitFor(() => {
+      expect(getByText(ENVIADO)).toBeTruthy();
+    });
+
+    expect(() => getByText(NAO_ENVIADO)).toThrow();
   });
 });
--- src/telas/Leilao/componentes/EnviaLance.js
+++ src/telas/Leilao/componentes/EnviaLance.js
@@ -41,6 +41,7 @@ export default function EnviaLance({ enviaLance, cor }) {
       keyboardType="decimal-pad"
     />
     <TouchableOpacity 
+      accessibilityHint="Enviar lance"
       onPress={validaEnvio}
       disabled={enviando}
       style={estilos.botao}>
